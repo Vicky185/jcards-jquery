a79f089e1177e77e5ba2edf37aa8aba5f81edb2b
Ubuntu <ubuntu@ip-172-31-90-110.ec2.internal>
2019-07-07 15:07:37 +0000

    mouseenter and leave functions

--- script.js
+++ script.js
@@ -1,8 +1,5 @@
 $(document).ready(function() {
 
-//	$("#button_effects1").on("click", function() {
-//		$("#button_effects1").hide('fast');
-//	});
 	$("#button_effects1").on("click", function() {
 		$("#para_toggle1").slideToggle('slow');
 	});
@@ -21,7 +18,49 @@ $(document).ready(function() {
 	$("#button_effects6").on("click", function() {
 		$("#para_toggle6").slideToggle('fast');
 	});
+	
+	
+	$("#button_effects1").mouseenter(function() {
+		$("#button_effects1").fadeTo(1000, 0.5);
+	});
+	$("#button_effects1").mouseleave(function() {
+		$("#button_effects1").fadeTo(1000, 1);
+	});
+	$("#button_effects2").mouseenter(function() {
+		$("#button_effects2").fadeTo(1000, 0.5);
+	});
+	$("#button_effects2").mouseleave(function() {
+		$("#button_effects2").fadeTo(1000, 1);
+	});
+	$("#button_effects1").mouseenter(function() {
+		$("#button_effects1").fadeTo(1000, 0.5);
+	});
+	$("#button_effects3").mouseleave(function() {
+		$("#button_effects3").fadeTo(1000, 1);
+	});
+	$("#button_effects4").mouseenter(function() {
+		$("#button_effects4").fadeTo(1000, 0.5);
+	});
+	$("#button_effects4").mouseleave(function() {
+		$("#button_effects4").fadeTo(1000, 1);
+	});
+	$("#button_effects5").mouseenter(function() {
+		$("#button_effects5").fadeTo(1000, 0.5);
+	});
+	$("#button_effects5").mouseleave(function() {
+		$("#button_effects5").fadeTo(1000, 1);
+	});
+	$("#button_effects6").mouseenter(function() {
+		$("#button_effects6").fadeTo(1000, 0.5);
+	});
+	$("#button_effects6").mouseleave(function() {
+		$("#button_effects6").fadeTo(1000, 1);
+	});
+	
 });
+
+
+
 
 //	$("p").click(function() {
 //		$("p").addClass("highlight_text");
